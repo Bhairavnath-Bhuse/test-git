a64c6024e8681c1ab5798b83be8058aebf73f906
Deep <deepamrutkar2943@gmail.com>
2025-05-12 12:25:09 +0530

    First commit

---
+++ demo/src/main/java/com/example/demo/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmployeeService {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	public Employee saveEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	public Optional<Employee> getEmployeeById(Long id) {
+		return employeeRepository.findById(id);
+	}
+}
